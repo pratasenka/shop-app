4ae00bf5318f0ec188a08bd5bae5816124dcbfe9
Anton Pratasenka <pratasenka@MacBook-Pro-Anton.local>
2022-10-23 16:56:52 +0300

    task-5 done

---
+++ import-service/serverless.ts
@@ -0,0 +1,58 @@
+import type { AWS } from "@serverless/typescript";
+
+import importProductsFile from "@functions/importProductsFile";
+import importFileParser from "@functions/importFileParser";
+import { BUCKET_NAME } from "src/utils";
+
+const serverlessConfiguration: AWS = {
+    service: "import-service",
+    frameworkVersion: "3",
+    plugins: ["serverless-esbuild"],
+    provider: {
+        name: "aws",
+        runtime: "nodejs14.x",
+        region: "eu-west-1",
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            shouldStartNameWithService: true,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+            NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+        },
+        iam: {
+            role: {
+                statements: [
+                    {
+                        Effect: "Allow",
+                        Action: ["s3:ListBucket"],
+                        Resource: `arn:aws:s3:::${BUCKET_NAME}`,
+                    },
+                    {
+                        Effect: "Allow",
+                        Action: ["s3:*"],
+                        Resource: `arn:aws:s3:::${BUCKET_NAME}/*`,
+                    },
+                ],
+            },
+        },
+    },
+    // import the function via paths
+    functions: { importProductsFile, importFileParser },
+    package: { individually: true },
+    custom: {
+        esbuild: {
+            bundle: true,
+            minify: false,
+            sourcemap: true,
+            exclude: ["aws-sdk"],
+            target: "node14",
+            define: { "require.resolve": undefined },
+            platform: "node",
+            concurrency: 10,
+        },
+    },
+};
+
+module.exports = serverlessConfiguration;
+
---
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -0,0 +1,34 @@
+import * as AWS from "aws-sdk";
+
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
+import { formatJSONResponse } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+import { BUCKET_NAME } from "../../utils";
+
+import schema from "./schema";
+
+const importProductsFile: ValidatedEventAPIGatewayProxyEvent<
+    typeof schema
+> = async (event) => {
+    const fileName = event.queryStringParameters.name;
+    const s3 = new AWS.S3({ region: "eu-west-1" });
+
+    console.log(fileName)
+
+    try {
+        const url = await s3.getSignedUrl("putObject", {
+            Bucket: BUCKET_NAME,
+            Key: `uploaded/${fileName}`,
+            ContentType: "text/csv",
+            // ACL: "public-read-write",
+        });
+
+        return formatJSONResponse({
+            signedURL: url,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const main = middyfy(importProductsFile);
